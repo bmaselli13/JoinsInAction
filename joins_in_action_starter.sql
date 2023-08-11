d5fba0fadce2755222f2d890a1a95d15d12a38a6
Brian Maselli <bmaselli@Brians-M2-MacBook-Air.local>
2023-08-11 08:58:22 -0700

    Finished Project

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -36,9 +36,10 @@ GROUP BY m.lead_studio ORDER BY favorite_count DESC LIMIT 1;
 -- EXPECTED RESULT: 52.21
 SELECT AVG(m.rotten_tomatoes) AS average_rotten_tomatoes FROM movies m INNER JOIN users u ON m.movie_id = u.favorite_movie_id;
 
-
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
+SELECT film, COUNT(*) AS duplicate_count FROM movies GROUP BY film HAVING COUNT(*) > 1;
+
 
